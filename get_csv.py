2c136b7ae7e31c320a7c0eab75de2f6bb7035121
Norawit Urailertprasert <norawit543@hotmail.com>
2017-10-30 16:52:52 +0700

    change to get fragment

--- get_csv.py
+++ get_csv.py
@@ -1,38 +1,68 @@
-import psycopg2
 import csv
+import psycopg2
+
 #db_name = "syn_c500_t15000_a3_al5"
-def get_syn(db_name, chunk_size, author_number, num_paper):
-  con = psycopg2.connect("dbname ='%s' user='cpehk01' host=/tmp/" %(db_name))
-  cur = con.cursor()
-  print(db_name+" "+str(chunk_size)+" "+str(author_number))
-  list_return = []
+db_name = "syn_eng_c600_t12000_a2_al5_sw600"
+con = psycopg2.connect("dbname ='%s' user='stylometry'" %(db_name))
+cur = con.cursor()
 
-  chunk_num = 1
-  for i in range(0, num_paper):   #number papers
-    for j in range(0, chunk_size):  #number chunks per paper (token_size/chunk_size)
-      list_feature = []
-      chunk_per_fragment = 0
-      try:
-        chunk_per_fragment = (j/(chunk_size/author_number))
-      except ZeroDivisionError:
-        print('error: didided by 0') 
-      list_feature.append(str((chunk_per_fragment+1)+(author_number*i))) #first number is chunk per fragment, and the last number is number of authors (aka a2)
-      #list_feature.append(str(((j/10)+1)+(3*i)))
-      print(str(chunk_per_fragment))
-      print(str(chunk_per_fragment+1)+" "+str(author_number*i))
-      list_feature.append(str(i+1))
-      list_feature.append(str(chunk_num))
-      cur.execute("SELECT value FROM features WHERE paper_id = '%s' AND chunk_id = '%s'", [i+1, chunk_num])
-      temp = cur.fetchall()
-      for k in range(0, len(temp)):
-        list_feature.append(temp[k][0])
-      list_return.append(list_feature)
-      chunk_num += 1
-  with open('./max/'+db_name+'.csv','w') as csvfile:
-    write = csv.writer(csvfile, delimiter=',')
-    for x in range(0, len(list_return)):
-      write.writerow(list_return[x])
+list_return = []
 
-#db_list = ['syn_eng_max_while_np1000_c1_t500_a2_al2_sw1']
-for db_name in db_list:
-  get_syn(db_name, int(db_name.split('_')[-4].split('t')[-1])/int(db_name.split('_')[-1].split('sw')[-1]), int(db_name.split('_')[-3].split('a')[-1]), int(db_name.split('_')[-6].split('np')[-1]))
+"""
+        fragment_size: number of chuncks in a fragment
+        offset: number of chucks between n and n+1 fragment
+        chunk_size: number of chunks
+"""
+def is_fragmentable(fragment_size,offset,chunk_size):
+        return ((chunk_size - fragment_size) / offset) % 1 == 0
+
+def get_fragments(fragment_size,offset,chunk_size):
+        if is_fragmentable(fragment_size,offset,chunk_size):
+                        return [tokens[x:x + fragment_size] for x in xrange(0, len(chunk_size), offset)]
+
+def get_num_fragment(fragment_size,offset,chunk_size):
+        return (chunk_size - fragment_size) / offset +1 
+
+def save_to_csv():
+        with open('./syn_a2_sw600.csv','w') as csvfile:
+                write = csv.writer(csvfile,delimiter=',')
+                for x in range(0,len(list_return)):
+                        write.writerow(list_return[x])
+
+
+def get_features(num_paper,fragment_size,offset,num_fragment):
+        fragment_count = 1
+        chunk_count = 1 + fragment_size
+        chunk_number = 1
+        for i in range(0,num_paper):
+                for j in range(fragment_count,fragment_count+num_fragment):
+                        chunk_count-=fragment_size
+                        for k in range(chunk_count,chunk_count+fragment_size):
+                                list_feature = []
+                                list_feature.append(str(j))
+                                list_feature.append(str(i+1))
+                                list_feature.append(str(chunk_number))
+                                cur.execute("SELECT value FROM features WHERE paper_id = '%s' AND chunk_id = '%s'",[i+1,k])
+                                temp = cur.fetchall()
+                                for l in range(0,len(temp)):
+                                        list_feature.append(temp[l][0])
+                                list_return.append(list_feature)
+                                chunk_count += 1
+                                chunk_number+=1
+                        chunk_count +=offset
+                chunk_count +=fragment_size - offset
+                fragment_count+=num_fragment
+
+if __name__ == '__main__':
+        fragment_size = 10
+        offset = 2
+        chunk_size = 20
+        num_paper = 500
+        if is_fragmentable(fragment_size,offset,chunk_size):
+                num_fragment = get_num_fragment(fragment_size,offset,chunk_size)
+                get_features(num_paper,fragment_size,offset,num_fragment)
+                save_to_csv()
+
+
+ 
+
