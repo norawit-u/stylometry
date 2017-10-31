92d00aacbe7f2172ec2f1d5313c5be4ed9058b87
Norawit Urailertprasert <norawit543@hotmail.com>
2017-10-31 11:11:22 +0700

    print num fragment

--- get_csv.py
+++ get_csv.py
@@ -144,5 +144,6 @@ if __name__ == '__main__':
     arg = parser_args()
     if is_fragmentable(arg.fragment_size, arg.offset, arg.chunk_size):
         num_fragment = get_num_fragment(arg.fragment_size, arg.offset, arg.chunk_size)
-        list_return = get_features(arg.num_paper, arg.fragment_size, arg.offset, num_fragment, arg.db_name)
+        print(num_fragment)
+        list_return = get_features(arg.num_paper, arg.fragment_size, arg.offset, num_fragment, arg.db_name[0])
         save_to_csv(list_return, arg.db_name[0])
