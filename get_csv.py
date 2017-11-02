b9d9ff5ec340a04a537341040114b704344cd842
Norawit Urailertprasert <norawit543@hotmail.com>
2017-11-02 14:23:09 +0700

    clean code

--- get_csv.py
+++ get_csv.py
@@ -1,40 +1,80 @@
-import psycopg2
 import csv
-#db_name = "syn_c500_t15000_a3_al5"
-def get_syn(db_name,chunk_size,author_number,num_paper):
-        con = psycopg2.connect("dbname ='%s' user='cpehk01' host=/tmp/" %(db_name))
-        cur = con.cursor()
-        print(db_name+" "+str(chunk_size)+" "+str(author_number))
-        list_return = []
+import argparse
+import psycopg2
 
-        chunk_num = 1
-        for i in range(0,num_paper):   #number papers
-                for j in range(0,chunk_size):    #number chunks per paper (token_size/chunk_size)
-                        list_feature = []
-                        chunk_per_fragment = 0
-                        try:
-                                chunk_per_fragment = (j/(chunk_size/author_number))
-                        except ZeroDivisionError:
-                                print('error: didided by 0')
-                        list_feature.append(str((chunk_per_fragment+1)+(author_number*i))) #first number is chunk per fragment, and the last number is number of authors (aka a2)
-                        #list_feature.append(str(((j/10)+1)+(3*i)))
-                        print(str(chunk_per_fragment))
-                        print(str(chunk_per_fragment+1)+" "+str(author_number*i))
-                        list_feature.append(str(i+1))
-                        list_feature.append(str(chunk_num))
-                        cur.execute("SELECT value FROM features WHERE paper_id = '%s' AND chunk_id = '%s'",[i+1,chunk_num])
-                        temp = cur.fetchall()
-                        for k in range(0,len(temp)):
-                                list_feature.append(temp[k][0])
-                        list_return.append(list_feature)
-                        chunk_num += 1
-        with open('./max/'+db_name+'.csv','w') as csvfile:
-                write = csv.writer(csvfile,delimiter=',')
-                for x in range(0,len(list_return)):
-                        write.writerow(list_return[x])
+def save_to_csv(list_return, name, fieldnames):
+    """
+        save data to csv file
+        Args:
+            list_return: a list of that we want to write to a csv file
+            name: name of a csv file
+            fieldnames: field names of the csv file (header)
+    """
+    with open(name + '.csv', 'w') as csvfile:
+        for name in fieldnames:
+            csvfile.write(name + ',')
+        write = csv.writer(csvfile, delimiter=',')
+        for x in range(0, len(list_return)):
+            write.writerow(list_return[x])
 
-db_list = ['syn_eng_max_while_np1000_c600_t8000_a2_al2_sw200','syn_eng_max_while_np1000_c600_t8000_a2_al3_sw200','syn_eng_max_while_np1000_c600_t8000_a2_al4_sw200','syn_eng_max_while_np1000_c600_t8000_a2_al5_sw200']
-#db_list = ['syn_eng_max_while_np1000_c100_t1000_a2_al2_sw1','syn_eng_max_while_np1000_c100_t1000_a2_al2_sw100','syn_eng_max_while_np1000_c100_t1000_a2_al2_sw200','syn_eng_max_while_np1000_c100_t1000_a2_al2_sw50','syn_eng_max_while_np1000_c100_t1_a2_al2_sw1','syn_eng_max_while_np1000_c100_t1_a2_al2_sw100','syn_eng_max_while_np1000_c100_t1_a2_al2_sw200','syn_eng_max_while_np1000_c100_t1_a2_al2_sw50','syn_eng_max_while_np1000_c100_t3000_a2_al2_sw1','syn_eng_max_while_np1000_c100_t3000_a2_al2_sw100','syn_eng_max_while_np1000_c100_t3000_a2_al2_sw200','syn_eng_max_while_np1000_c100_t3000_a2_al2_sw50','syn_eng_max_while_np1000_c100_t500_a2_al2_sw1','syn_eng_max_while_np1000_c100_t500_a2_al2_sw100','syn_eng_max_while_np1000_c100_t500_a2_al2_sw200','syn_eng_max_while_np1000_c100_t500_a2_al2_sw50','syn_eng_max_while_np1000_c1_t1000_a2_al2_sw1','syn_eng_max_while_np1000_c1_t1000_a2_al2_sw100','syn_eng_max_while_np1000_c1_t1000_a2_al2_sw200','syn_eng_max_while_np1000_c1_t1000_a2_al2_sw50','syn_eng_max_while_np1000_c1_t10_a2_al2_sw1','syn_eng_max_while_np1000_c1_t1_a2_al2_sw1','syn_eng_max_while_np1000_c1_t1_a2_al2_sw100','syn_eng_max_while_np1000_c1_t1_a2_al2_sw200','syn_eng_max_while_np1000_c1_t1_a2_al2_sw50','syn_eng_max_while_np1000_c1_t2_a2_al2_sw1','syn_eng_max_while_np1000_c1_t3000_a2_al2_sw1','syn_eng_max_while_np1000_c1_t3000_a2_al2_sw100','syn_eng_max_while_np1000_c1_t3000_a2_al2_sw200','syn_eng_max_while_np1000_c1_t3000_a2_al2_sw50','syn_eng_max_while_np1000_c1_t3_a2_al2_sw1','syn_eng_max_while_np1000_c1_t4_a2_al2_sw1','syn_eng_max_while_np1000_c1_t500_a2_al2_sw1','syn_eng_max_while_np1000_c1_t500_a2_al2_sw100','syn_eng_max_while_np1000_c1_t500_a2_al2_sw200','syn_eng_max_while_np1000_c1_t500_a2_al2_sw50','syn_eng_max_while_np1000_c1_t5_a2_al2_sw1','syn_eng_max_while_np1000_c200_t1000_a2_al2_sw1','syn_eng_max_while_np1000_c200_t1000_a2_al2_sw100','syn_eng_max_while_np1000_c200_t1000_a2_al2_sw200','syn_eng_max_while_np1000_c200_t1000_a2_al2_sw50','syn_eng_max_while_np1000_c200_t12000_a2_al2_sw200','syn_eng_max_while_np1000_c200_t12000_a2_al2_sw300','syn_eng_max_while_np1000_c200_t12000_a2_al2_sw400','syn_eng_max_while_np1000_c200_t1_a2_al2_sw1','syn_eng_max_while_np1000_c200_t1_a2_al2_sw100','syn_eng_max_while_np1000_c200_t1_a2_al2_sw200','syn_eng_max_while_np1000_c200_t1_a2_al2_sw50','syn_eng_max_while_np1000_c200_t3000_a2_al2_sw1','syn_eng_max_while_np1000_c200_t3000_a2_al2_sw100','syn_eng_max_while_np1000_c200_t3000_a2_al2_sw200','syn_eng_max_while_np1000_c200_t3000_a2_al2_sw300','syn_eng_max_while_np1000_c200_t3000_a2_al2_sw400','syn_eng_max_while_np1000_c200_t3000_a2_al2_sw50','syn_eng_max_while_np1000_c200_t500_a2_al2_sw1','syn_eng_max_while_np1000_c200_t500_a2_al2_sw100','syn_eng_max_while_np1000_c200_t500_a2_al2_sw200','syn_eng_max_while_np1000_c200_t500_a2_al2_sw50','syn_eng_max_while_np1000_c200_t6000_a2_al2_sw200','syn_eng_max_while_np1000_c200_t6000_a2_al2_sw300','syn_eng_max_while_np1000_c200_t6000_a2_al2_sw400','syn_eng_max_while_np1000_c300_t12000_a2_al2_sw200','syn_eng_max_while_np1000_c300_t12000_a2_al2_sw300','syn_eng_max_while_np1000_c300_t12000_a2_al2_sw400','syn_eng_max_while_np1000_c300_t3000_a2_al2_sw200','syn_eng_max_while_np1000_c300_t3000_a2_al2_sw300','syn_eng_max_while_np1000_c300_t3000_a2_al2_sw400','syn_eng_max_while_np1000_c300_t6000_a2_al2_sw200','syn_eng_max_while_np1000_c300_t6000_a2_al2_sw300','syn_eng_max_while_np1000_c300_t6000_a2_al2_sw400','syn_eng_max_while_np1000_c400_t12000_a2_al2_sw200','syn_eng_max_while_np1000_c400_t12000_a2_al2_sw300','syn_eng_max_while_np1000_c400_t12000_a2_al2_sw400','syn_eng_max_while_np1000_c400_t3000_a2_al2_sw200','syn_eng_max_while_np1000_c400_t3000_a2_al2_sw300','syn_eng_max_while_np1000_c400_t3000_a2_al2_sw400','syn_eng_max_while_np1000_c400_t6000_a2_al2_sw200','syn_eng_max_while_np1000_c400_t6000_a2_al2_sw300','syn_eng_max_while_np1000_c400_t6000_a2_al2_sw400','syn_eng_max_while_np1000_c50_t1000_a2_al2_sw1','syn_eng_max_while_np1000_c50_t1000_a2_al2_sw100','syn_eng_max_while_np1000_c50_t1000_a2_al2_sw200','syn_eng_max_while_np1000_c50_t1000_a2_al2_sw50','syn_eng_max_while_np1000_c50_t1_a2_al2_sw1','syn_eng_max_while_np1000_c50_t1_a2_al2_sw100','syn_eng_max_while_np1000_c50_t1_a2_al2_sw200','syn_eng_max_while_np1000_c50_t1_a2_al2_sw50','syn_eng_max_while_np1000_c50_t3000_a2_al2_sw1','syn_eng_max_while_np1000_c50_t3000_a2_al2_sw100','syn_eng_max_while_np1000_c50_t3000_a2_al2_sw200','syn_eng_max_while_np1000_c50_t3000_a2_al2_sw50','syn_eng_max_while_np1000_c50_t500_a2_al2_sw1','syn_eng_max_while_np1000_c50_t500_a2_al2_sw100','syn_eng_max_while_np1000_c50_t500_a2_al2_sw200','syn_eng_max_while_np1000_c50_t500_a2_al2_sw50','syn_eng_max_while_np1000_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1500_c600_t12000_a2_al5_sw600','syn_eng_max_while_np1500_c600_t12000_a4_al5_sw600','syn_eng_max_while_np1500_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1600_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1648_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1680_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1683_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1684_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1685_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1686_c600_t12000_a5_al5_sw600','syn_eng_max_while_np1689_c600_t12000_a5_al5_sw600','syn_eng_max_while_np2000_c600_t12000_a5_al5_sw600','syn_eng_max_while_np3000_c600_t12000_a5_al5_sw600','syn_eng_max_while_np4000_c600_t12000_a5_al5_sw600','syn_eng_max_while_np5000_c600_t12000_a5_al5_sw600','syn_eng_max_while_np6000_c600_t12000_a5_al5_sw600']
-#db_list = ['syn_eng_max_while_np1000_c1_t500_a2_al2_sw1']
-for db_name in db_list:
-        get_syn(db_name,int(db_name.split('_')[-4].split('t')[-1])/int(db_name.split('_')[-1].split('sw')[-1]),int(db_name.split('_')[-3].split('a')[-1]),int(db_name.split('_')[-6].split('np')[-1]))
+
+def get_syn(db_name, chunk_size, author_number, num_paper):
+    """
+        get the feature from the database
+        Args:
+            db_name: database name
+            chunk_size: number of chunk in a fragment
+            author_number: number of author in paper
+            num_paper: number of paper
+        Returns:
+            List of features
+    """
+    con = psycopg2.connect("dbname ='%s' user='cpehk01' host=/tmp/" % (db_name))
+    cur = con.cursor()
+    print(db_name + " " + str(chunk_size) + " " + str(author_number))
+    list_return = []
+
+    chunk_num = 1
+    for i in range(0, num_paper):  # number papers
+        for j in range(0, chunk_size):  # number chunks per paper (token_size/chunk_size)
+            list_feature = []
+            chunk_per_fragment = 0
+            try:
+                chunk_per_fragment = (j / (chunk_size / author_number))
+            except ZeroDivisionError:
+                print('error: didided by 0')
+            list_feature.append(str((chunk_per_fragment + 1) + (
+            author_number * i)))  # first number is chunk per fragment, and the last number is number of authors (aka a2)
+            # list_feature.append(str(((j/10)+1)+(3*i)))
+            print(str(chunk_per_fragment))
+            print(str(chunk_per_fragment + 1) + " " + str(author_number * i))
+            list_feature.append(str(i + 1))
+            list_feature.append(str(chunk_num))
+            cur.execute("SELECT value FROM features WHERE paper_id = '%s' AND chunk_id = '%s'", [i + 1, chunk_num])
+            temp = cur.fetchall()
+            for k in range(0, len(temp)):
+                list_feature.append(temp[k][0])
+            list_return.append(list_feature)
+            chunk_num += 1
+    return list_return
+
+
+def parser_args():
+    parser = argparse.ArgumentParser(description='Get a stylometry synthetic data.')
+    parser.add_argument('--chunk_size', type=int, help='number of chunk in a fragment')
+    parser.add_argument('--num_chunk', type=int, help='number of chunk in a fragment')
+    parser.add_argument('--num_paper', type=int, help='number of paper')
+    parser.add_argument('--db_name', type=str, nargs='*', help="database name that want to get")
+    parser.add_argument('--out_path', type=str, help="output path", default='.')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    field_names = ['fragment_id', 'paper_id', 'chunk_id']
+    field_names.extend(['fragment_' + str(i) for i in range(1, 58)])
+    arg = parser_args()
+    list_return = get_syn(arg.db_name, int(arg.db_name.split('_')[-4].split('t')[-1]) / int(
+        arg.db_name.split('_')[-1].split('sw')[-1]),
+                          int(arg.db_name.split('_')[-3].split('a')[-1]),
+                          int(arg.db_name.split('_')[-6].split('np')[-1]))
+    save_to_csv(list_return, arg.out_path + "/" + str(arg.fragment_size) + '_' + arg.db_name[0], field_names)
