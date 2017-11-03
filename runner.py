8debc12a4494d6c9ee0c77f15a7b890949f50fce
Norawit Urailertprasert <norawit543@hotmail.com>
2017-11-03 12:55:36 +0700

    excute

--- runner.py
+++ runner.py
@@ -29,11 +29,12 @@ def execute(command):
 
 def cross(db_name, path, num_paper, n_fold):
     folds = gen_fold(num_paper, n_fold)
-    print(folds)
+    # print(folds)
     for key, fold in zip([i for i in range(0,len(folds))], folds):
-        print(fold)
+        # print(folds)
         get_csv = command_get_csv(db_name, path + '/csv', fold, '_n'+str(key))
-
+        print(get_csv)
+        execute(get_csv)
 
 
 def parser_args():
