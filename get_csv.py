89560887c7b3e5135d953239a8022d6822dff19c
Norawit Urailertprasert <norawit543@hotmail.com>
2017-11-09 14:13:25 +0700

    change to papers

--- get_csv.py
+++ get_csv.py
@@ -143,7 +143,7 @@ def parser_args():
     parser.add_argument('--num_chunk', type=int, help='number of chunk in a fragment')
     parser.add_argument('--num_chunk_per_section', type=int, help='number of chunk in a section')
 
-    parser.add_argument('--num_paper', type=int, help='number of paper')
+    parser.add_argument('--papers', type=int, nargs='*', help='number of paper')
     parser.add_argument('--offset', type=int, help="number of chunks between n and n+1 fragment")
     parser.add_argument('--db_name', type=str, nargs='*', help="database name that want to get")
     parser.add_argument('--out_path', type=str, help="output path", default='.')
