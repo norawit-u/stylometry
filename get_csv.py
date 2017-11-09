faf64f72eb4ab314509e3cbd3aae1e383d56cdef
Norawit Urailertprasert <norawit543@hotmail.com>
2017-11-09 17:10:06 +0700

    add more debug

--- get_csv.py
+++ get_csv.py
@@ -123,6 +123,9 @@ def get_features(papers, num_chunk_per_fragment, offset, num_fragment, db_name):
                 list_feature.append(str(j))  # fragment id
                 list_feature.append(str(i + 1))  # paper id
                 list_feature.append(str(i))  # chunk id
+                list_feature.append(str(fragment_count))
+                list_feature.append(str(chunk_count))
+                list_feature.append(str(chunk_number))
                 cur.execute("SELECT value FROM features WHERE paper_id = '%s' AND chunk_id = '%s'", [i + 1, k])
                 temp = cur.fetchall()
                 for l in range(0, len(temp)):
