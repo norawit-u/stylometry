097acc5ca324b1d687eabba136d890cecadd848d
Norawit Urailertprasert <norawit543@hotmail.com>
2017-11-15 15:53:20 +0700

    add arg

--- visualize.py
+++ visualize.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import ast
+import argparse
 
 PATH = "syn_eng_max_while_np1000_c600_t8000_a2_al2_sw200_n0/"
 
@@ -25,6 +26,16 @@ def get_input():
 def plot(x):
     fig = plt.hist(x, normed=0)
     plt.savefig('temp.png')
+
+def parser_args():
+    """
+    init argument parsing
+    :return: argument
+    """
+    parser = argparse.ArgumentParser(description='Get a stylometry synthetic data.')
+    parser.add_argument('--num_paper', type=int, help='number of paper')
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
     data = get_input()
